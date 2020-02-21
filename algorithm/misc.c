976ae086d274957188a7fb1ff8413facbf7e263b
hooollin <lan-cin@qq.com>
2020-02-21 11:38:42 +0800

    字符串插入o(n)时间复杂度算法

--- algorithm/misc.c
+++ algorithm/misc.c
@@ -24,12 +24,54 @@ int gcd(int a, int b)
     }
     return a;
 }
+/**
+ * 字符串双指针替换算法
+ * 时间复杂度O(n)
+ * 很牛逼
+*/
+void replace(char *str, char ch, char *repStr)
+{
+    int strLen = 0, repStrLen = 0, chFre = 0, newLen = 0;
+    for (; str[strLen] != '\0'; strLen++)
+    {
+        if (str[strLen] == ch)
+        {
+            chFre++;
+        }
+    }
+
+    for (; repStr[repStrLen] != '\0'; repStrLen++)
+        ;
+    newLen = strLen + chFre * repStrLen;
+    char t[newLen + 1];
+    int i = 0, j = 0;
+    while (i < strLen)
+    {
+        if (str[i] == ch)
+        {
+            for (int k = 0; k < repStrLen; k++)
+            {
+                t[j++] = repStr[k];
+            }
+            i += 1;
+        }
+        else
+        {
+            t[j++] = str[i++];
+        }
+    }
+    printf("%s", t);
+}
 
 int main()
 {
-    srand(time(0));
-    int a = rand() % 100;
-    int b = rand() % 100;
-    printf("a=%d, b=%d, res=%d\n", a, b, gcd(a, b));
+    //    srand(time(0));
+    //int a = rand() % 100;
+    //int b = rand() % 100;
+    //printf("a=%d, b=%d, res=%d\n", a, b, gcd(a, b));
+    //return 0;
+    char *str = "I'm happy to know that";
+    char *repStr = "%20";
+    replace(str, ' ', repStr);
     return 0;
 }
