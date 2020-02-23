f4789689c8956b61032a2638f173fa45cbfd0b40
hooollin <lan-cin@qq.com>
2020-02-23 22:49:15 +0800

    添加交换两个结点的代码

--- algorithm/list.c
+++ algorithm/list.c
@@ -7,6 +7,64 @@ typedef struct node
     struct node *next;
 } LinkedList, ListNode;
 
+LinkedList *buildLinkedList();
+void append(LinkedList *head, int data);
+void insertHead(LinkedList *head, int data);
+void insert(LinkedList *head, ListNode *target, int data);
+void idxInsert(LinkedList *head, int idx, int data);
+int findIdx(LinkedList *head, int data);
+ListNode *findNode(LinkedList *head, int data);
+void printList(LinkedList *head);
+void _swap(LinkedList *head, int val1, int val2);
+void swap(LinkedList *head, ListNode *node1, ListNode *node2);
+
+/**
+ * 通过值来找到Node并且交换两个Node
+ * 不保证元素重复的时候交换的是你想象中的元素
+ */
+void _swap(LinkedList *head, int data1, int data2)
+{
+    ListNode *node1 = head->next, *node2 = head->next;
+    for (; node1->data != data1; node1 = node1->next)
+        ;
+    for (; node2->data != data2; node2 = node2->next)
+        ;
+    swap(head, node1, node2);
+}
+/**
+ * 没有头结点的swap要考虑是不是第一个结点，带头结点的就不用了
+ * 同时要考虑两个结点是否相邻
+ */
+void swap(LinkedList *head, ListNode *node1, ListNode *node2)
+{
+    ListNode *prev1 = head, *curr1 = head->next, *prev2 = head, *curr2 = head->next, *next;
+    for (; curr1 != node1; prev1 = prev1->next, curr1 = curr1->next)
+        ;
+    for (; curr2 != node2; prev2 = prev2->next, curr2 = curr2->next)
+        ;
+    if (curr1->next == curr2)
+    {
+        next = curr2->next;
+        curr2->next = curr1;
+        curr1->next = next;
+        prev1->next = curr2;
+    }
+    else if (curr2->next == curr1)
+    {
+        next = curr1->next;
+        curr1->next = curr2;
+        curr2->next = next;
+        prev2->next = curr1;
+    }
+    else
+    {
+        next = curr1->next;
+        curr1->next = curr2->next;
+        curr2->next = next;
+        prev1->next = curr2;
+        prev2->next = curr1;
+    }
+}
 LinkedList *buildLinkedList()
 {
     ListNode *head = (ListNode *)malloc(sizeof(ListNode)), *curr = head;
@@ -173,7 +231,8 @@ void printList(LinkedList *head)
 int main()
 {
     LinkedList *head = buildLinkedList();
-    idxInsert(head, 0, 10000);
+    printList(head);
+    _swap(head, 10, 20);
     printList(head);
     return 0;
 }
