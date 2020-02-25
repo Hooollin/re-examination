d2ae9c8287e471663d300b024f1b564a85363294
hooollin <lan-cin@qq.com>
2020-02-26 00:10:26 +0800

    add implementation of memcpy memmove memset

--- algorithm/_string.c
+++ algorithm/_string.c
@@ -36,10 +36,55 @@ int memcmp(const void *str1, const void *str2, size_t n)
     return 0;
 }
 
+void *memcpy(void *dest, const void *src, size_t n)
+{
+    const char *prt1 = src;
+    char *prt2 = dest;
+    size_t i = 0;
+    for (; i < n; i++, prt1++, prt2++)
+    {
+        *prt2 = *prt1;
+    }
+    return dest;
+}
+
+void *memmove(void *dest, const void *src, size_t n)
+{
+    if (src >= dest || (char *)src + n < (char *)dest)
+    {
+        size_t i = 0;
+        const char *prt1 = src;
+        char *prt2 = dest;
+        for (; i < n; i++, prt1++, prt2++)
+        {
+            *prt2 = *prt1;
+        }
+    }
+    else
+    {
+        char *l = (char *)src + n;
+        size_t len = ((char *)dest - (char *)src);
+        while (l != (char *)src - 1)
+        {
+            *(l + len) = *l;
+            l--;
+        }
+    }
+    return dest;
+}
+
+void *memset(void *str, int c, size_t n)
+{
+    char *p = str;
+    for (size_t i = 0; i < n; i++, *(p++) = c)
+        ;
+    return p;
+}
 int main()
 {
-    char str1[10] = {"ello"};
-    char str2[10] = {"ell1"};
-    printf("%d", memcmp(str1, str2, 3));
+    char str1[10] = {"hello"};
+    char str2[10] = {""};
+    memset(str1, 'a', 2);
+    printf("%s", str1);
     return 0;
 }
--- algorithm/_string.h
+++ algorithm/_string.h
@@ -9,4 +9,14 @@ size_t strlen(const char *str);
 
 // 把 str1 和 str2 的前 n 个字节进行比较。
 int memcmp(const void *str1, const void *str2, size_t n);
+
+//从 src 复制 n 个字符到 dest，但是源和目的的内存区域不能重叠。
+void *memcpy(void *dest, const void *src, size_t n);
+
+//从 src 复制 n 个字符到 dest,源和目的的内存区域可以重叠。
+void *memmove(void *dest, const void *src, size_t n);
+
+//复制字符 c（一个无符号字符）到参数 str 所指向的字符串的前 n 个字符。
+void *memset(void *str, int c, size_t n);
+
 #endif
