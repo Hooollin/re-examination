20bdd0a5a08031a0f1dd04f484152accc57a0a44
hooollin <lan-cin@qq.com>
2020-03-04 17:18:33 +0800

    generic max

--- algorithm/_macro.c
+++ algorithm/_macro.c
@@ -4,10 +4,14 @@
 #define END }
 #define back return
 #define start printf("executed on %s at %s, everything seems to be fine.\n", __DATE__, __TIME__)
-#define print(x) printf(#x " = %d\n", (x))
+#define print(x) printf(#x " = %lf\n", (x))
 /* 可能会有副作用 */
 #define max(x, y) ((x) > (y) ? (x) : (y))
-
+#define GENERIC_MAX(type)           \
+    type type##_max(type a, type b) \
+    {                               \
+        return a > b ? a : b;       \
+    }
 int main()
     BEGIN
 
@@ -16,6 +20,8 @@ int main()
 int sn = 21, bn = 20;
 print(max(sn++, bn));
 print(sn);
+GENERIC_MAX(float);
+print(float_max(20.0, 30.0));
 
 back 0;
 END
