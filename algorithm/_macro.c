ba695de4908d3a31e5dfb1875eb0e981cb9ea09b
hooollin <lan-cin@qq.com>
2020-03-04 17:10:59 +0800

    有副作用的max

--- algorithm/_macro.c
+++ algorithm/_macro.c
@@ -4,8 +4,18 @@
 #define END }
 #define back return
 #define start printf("executed on %s at %s, everything seems to be fine.\n", __DATE__, __TIME__)
+#define print(x) printf(#x " = %d\n", (x))
+/* 可能会有副作用 */
+#define max(x, y) ((x) > (y) ? (x) : (y))
+
 int main()
     BEGIN
+
     start;
+/* 有副作用的max */
+int sn = 21, bn = 20;
+print(max(sn++, bn));
+print(sn);
+
 back 0;
 END
